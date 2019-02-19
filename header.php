7244e1a2c098689d9a9fff65bcc39cd4fa7423ec
tallg <tallg@MSI.lan>
2019-02-18 21:06:02 -0800

    Project Start

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="topbar">
+    <div class="navbar">
+        <img src="res/logoTextClear.png" class="headerimg"> 
+        <a href="index.php"><i class="fa fa-fw fa-home"></i> Home</a> 
+        <a href="#"><i class="fas fa-upload"></i> Upload</a> 
+        <form action="#">
+            <div class="rightSearch">
+                <input type="text" placeholder="Search..">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+        </form>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" crossorigin="anonymous">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans+Caption" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+
+        
+        <link rel="icon" href="res/logo.png">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <title>cSplit Home</title>
+    </head>
+    <body>
+        <?php require('header.php'); ?>
+        <div class="modulebox">
+            <img src="res/logoTextClear.png" >
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing 
+                elit. Aenean commodo ligula eget dolor. Aenean massa 
+                <strong>strong</strong>. Cum sociis natoque penatibus 
+                et magnis dis parturient montes, nascetur ridiculus 
+                mus. Donec quam felis, ultricies nec, pellentesque 
+                eu, pretium quis, sem. Nulla consequat massa quis 
+                enim. Donec pede justo, fringilla vel, aliquet nec, 
+                vulputate eget, arcu. In enim justo, rhoncus ut, 
+                imperdiet a, venenatis vitae, justo. Nullam dictum 
+                felis eu pede <a class="external ext" href="#">link</a> 
+                mollis pretium. Integer tincidunt. Cras dapibus. 
+                Vivamus elementum semper nisi. Aenean vulputate 
+                eleifend tellus. Aenean leo ligula, porttitor eu, 
+                consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
+                dapibus in, viverra quis, feugiat a, tellus. Phasellus 
+                viverra nulla ut metus varius laoreet. Quisque rutrum. 
+                Aenean imperdiet. Etiam ultricies nisi vel augue. 
+                Curabitur ullamcorper ultricies nisi.
+            </p>
+        </div>
+    </body>
+</html>
